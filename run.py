a7d0fa9b5f9764a4a10582a7ab7b2aad4b68b424
aokealy <aokealy@gmail.com>
2022-08-01 16:52:52 +0000

    add functions while pass for each one

--- run.py
+++ run.py
@@ -3,5 +3,32 @@ import random
 LENGTH_OF_SHIPS = [2,3,3,4,5] # Length of ships
 PLAYER_BOARD = [[" "] * 8 for i in range(8)] #the player board
 COMPUTER_BOARD = [[" "] * 8 for i in range(8)] 
+PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
+COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
+LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7} #list for letters to numbers
+
+def print_board(board):
+    pass
+
+def place_ships(board):
+    pass
+
+def check_ship_fit():
+    pass
+
+def ship_overlap():
+    pass
+
+def user_input():
+    pass
+
+def count_hit_ships():
+    pass
+
+def turn(board):
+    pass
+
+while True:
+
 
 
