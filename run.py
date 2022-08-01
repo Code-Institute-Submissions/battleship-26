adfaeb8fe367fc53b9d3c4b3f69935e69b6774fd
aokealy <aokealy@gmail.com>
2022-08-01 17:02:58 +0000

    loop through the board using string and decimal

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ LETTERS_TO_NUMBERS = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7} #l
 def print_board(board):
     print("  A B C D E F G H")
     print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board: #loop through board
+        print("%d|%s|" % (row_number, "|".join(row))) #decimal and string print
+        row_number += 1
 
 def place_ships(board):
     pass
